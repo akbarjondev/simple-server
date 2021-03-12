4ab09dc6e52b7814a35bd4170554166b9bdfb8ae
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-12 20:46:49 +0500

    Add endpoints

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require('express')
+const { fetch } = require('./src/db/db.js')
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
+app.get('/', (req, res) => {
+	res.send('Hello world!')
+})
+
+app.get('/users', async (_, res) => {
+	
+	const users = await fetch(`
+		select
+			username,
+			age,
+			is_student
+		from 
+			users
+	`)
+
+	res.send(users)
+
+})
 
 app.listen(PORT, () => console.log(`ready at http://localhost:${PORT}`))
